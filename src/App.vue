392a74cdfbd44a2d1c4e76877e6d814fa7adea42
i1 <i1.kim@symfony.com>
2019-03-24 19:40:24 +0200

    Template of select

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
 
+    <section class="custom-select">
+      <div class="list-items">
+        <div class="item" v-for="item in listItem">{{item.title}}</div>
+      </div>
+      <div class="search">
+        <input type="text" name="" value="">
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -10,7 +19,13 @@
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      listItem: [
+        {id:1, title:'Option 1', value:'Value1'},
+        {id:2, title:'Option 2', value:'Value2'},
+        {id:3, title:'Option 3', value:'Value3'},
+        {id:4, title:'Option 4', value:'Value4'},
+      ]
     }
   }
 }
@@ -30,17 +45,23 @@ h1, h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.custom-select {
+  position: relative;
+  width: 300px;
+  margin: 0 auto;
+  & input[type="text"] {
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    display: block;
+    padding: 0 6px;
+  }
+  & .list-items {
+    position: absolute;
+    top: 45px;
+    left: 0;
+    width: 100%;
+    border: 1px solid green;
+  }
 }
 </style>
